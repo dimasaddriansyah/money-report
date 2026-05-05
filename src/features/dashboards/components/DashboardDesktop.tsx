4275e7e241ef8026acdca14e4d0a1cad3e970b63
Dimas Addriansyah Pamungkas <dimasaddriansyah14@gmail.com>
2026-05-05 14:09:39 +0700

    feature/filter-dashboard-dekstop-page

--- src/features/dashboards/components/DashboardDesktop.tsx
+++ src/features/dashboards/components/DashboardDesktop.tsx
@@ -10,6 +10,9 @@ import { useDashboardData } from "../hooks/useDashboardData";
 import type { Transaction } from "../../transactions/types/transaction";
 import type { Account } from "../../accounts/types/account";
 import type { Category } from "../../categories/types/category";
+import { useState } from "react";
+import { PlusSignIcon } from "hugeicons-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   transactions: Transaction[];
@@ -18,21 +21,96 @@ type Props = {
   refetch: () => void;
 }
 
+type Period = "year" | "month" | "week" | "yesterday";
+
+
 export default function DashboardDesktop({ transactions, accounts, categories, refetch }: Props) {
+  const navigate = useNavigate();
+  const [period, setPeriod] = useState<Period>("month");
+  const filteredTransactions = filterTransactionsByPeriod(transactions, period);
   const {
     summary,
     accountsWithBalance,
     dailyExpense,
     topExpenses,
   } = useDashboardData({
-    transactions,
+    transactions: filteredTransactions,
     accounts,
     categories,
     refetch,
   });
 
+  function filterTransactionsByPeriod(transactions: Transaction[], period: Period) {
+    const now = new Date();
+
+    return transactions.filter((trx) => {
+      const trxDate = new Date(trx.date);
+
+      switch (period) {
+        case "year":
+          return trxDate.getFullYear() === now.getFullYear();
+
+        case "month":
+          return (
+            trxDate.getFullYear() === now.getFullYear() &&
+            trxDate.getMonth() === now.getMonth()
+          );
+
+        case "week": {
+          const day = now.getDay();
+          const diff = now.getDate() - day + (day === 0 ? -6 : 1);
+          const firstDayOfWeek = new Date(now);
+          firstDayOfWeek.setDate(diff);
+
+          return trxDate >= firstDayOfWeek && trxDate <= now;
+        }
+
+        case "yesterday": {
+          const yesterday = new Date(now);
+          yesterday.setDate(now.getDate() - 1);
+
+          return (
+            trxDate.getFullYear() === yesterday.getFullYear() &&
+            trxDate.getMonth() === yesterday.getMonth() &&
+            trxDate.getDate() === yesterday.getDate()
+          );
+        }
+
+        default:
+          return true;
+      }
+    });
+  }
+
   return (
     <DashboardLayout>
+      <section className="flex items-center justify-between">
+        <section className="flex w-fit p-1 bg-slate-100 border border-slate-200 rounded-lg">
+          {[
+            { label: "Year", value: "year" },
+            { label: "Month", value: "month" },
+            { label: "Week", value: "week" },
+            { label: "Yesterday", value: "yesterday" },
+          ].map((item) => (
+            <button
+              key={item.value}
+              onClick={() => setPeriod(item.value as Period)}
+              className={`px-4 py-2 text-sm transition cursor-pointer rounded-md
+                ${period === item.value
+                  ? "bg-white text-black font-semibold border border-slate-200"
+                  : "text-slate-400 hover:bg-slate-200"}`}>
+              {item.label}
+            </button>))}
+        </section>
+        <button
+          onClick={() => navigate('/transaction/create')}
+          className="flex items-center px-5 py-2.5 gap-2 bg-slate-900 hover:bg-slate-800 text-white text-sm font-medium rounded-lg cursor-pointer">
+          <PlusSignIcon size={16} />
+          <span>Create Transaction</span>
+        </button>
+      </section>
+
+
       <DashboardSectionSummary summary={summary} />
 
       <DashboardSectionAccountBalanceSummary accounts={accountsWithBalance} autoScroll={true} />

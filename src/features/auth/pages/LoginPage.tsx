dc0306387fce89f030bb423a19c7863d232fe042
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2026-07-12 18:17:54 +0700

    style/style-login-page

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import { login } from "../services/AuthService";
 import { useAuth } from "../hooks/useAuth";
 import cashflow from "../../../assets/cashflow.png"
 import { toast } from "sonner";
-import { Key02Icon, Mail01Icon } from "hugeicons-react";
+import { Key02Icon, Mail01Icon, ViewIcon, ViewOffSlashIcon } from "hugeicons-react";
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -15,7 +15,7 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
 
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
     if (user) {
@@ -28,7 +28,6 @@ export default function LoginPage() {
 
     try {
       setLoading(true);
-      setError("");
 
       await login(email, password);
 
@@ -44,16 +43,13 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-100">
-
+    <div className="flex flex-col gap-8 min-h-screen items-center justify-center bg-slate-100">
+      <img src={cashflow} alt="Banner" className="w-20 h-20" />
       <form
         onSubmit={handleSubmit}
-        className="w-full max-w-lg rounded-xl bg-white p-8 shadow-lg space-y-4">
+        className="w-full max-w-sm sm:max-w-lg  rounded-xl bg-white p-8 shadow-lg space-y-4">
 
-        <div className="flex flex-col items-center justify-center gap-2">
-          <img src={cashflow} alt="Banner" className="w-12 h-12" />
-          <h1 className="text-2xl font-bold">Login</h1>
-        </div>
+        <h1 className="text-2xl font-bold text-center">Login</h1>
 
         <div id="email" className="flex-1">
           <label className="block text-sm font-medium text-black mb-1">Email</label>
@@ -70,18 +66,32 @@ export default function LoginPage() {
           </div>
         </div>
 
-         <div id="password" className="flex-1">
+        <div id="password" className="flex-1">
           <label className="block text-sm font-medium text-black mb-1">Password</label>
-          <div
-            className="relative flex items-center justify-center">
+
+          <div className="relative flex items-center">
             <div className="absolute left-4 pointer-events-none"><Key02Icon className="text-slate-400" size={20} /></div>
+
             <input
               value={password}
-              type="password"
+              type={showPassword ? "text" : "password"}
               onChange={(e) => setPassword(e.target.value)}
-              className={`block w-full ps-13 pe-3 py-3 text-base rounded-xl border ${password ? "text-black" : "text-slate-400"} bg-white border-slate-300 focus:outline-none focus:ring-2 focus:ring-black placeholder:text-slate-400 transition appearance-none`}
+              className={`block w-full ps-13 pe-13 py-3 text-base rounded-xl border bg-white border-slate-300 focus:outline-none focus:ring-2 focus:ring-black placeholder:text-slate-400 transition
+                 ${password ? "text-black" : "text-slate-400"}`}
               placeholder="Input your password"
-              required />
+              required/>
+
+            <button
+              type="button"
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute right-4 text-slate-400 hover:text-black cursor-pointer">
+              {showPassword ? (
+                <ViewIcon size={20} />
+              ) : (
+                <ViewOffSlashIcon size={20} />
+              )}
+            </button>
+
           </div>
         </div>
 
